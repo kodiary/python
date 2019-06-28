d3ca9feb3a4eeeef771c142986868903c859c7e4
Anwar Ali <abuyusuf2045@gmail.com>
2019-06-28 22:41:22 +0545

    Day 2

---
+++ day2/test.py
@@ -0,0 +1,23 @@
+# print('hello world')
+# print(2)
+
+# a = 7
+# b = 3
+#
+# f = a ** b
+# print(f)
+
+#print(b+a)
+
+# c = "Hello world"
+#print(c)
+
+
+# d = True
+# e = False
+
+g = "My name is"
+h = "Anwar Ali"
+
+i = g + " " + h
+print(i)
